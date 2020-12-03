d3407a9fb758d2407bc7482a0a1b5e66948c825a
docnick <nicholas.larusso@gmail.com>
2020-12-03 17:22:12 -0500

    initial implementation of elbo, still needs verification/debugging and a lot of clean-up

--- gmm_variational_inference.py
+++ gmm_variational_inference.py
@@ -10,15 +10,18 @@ logging.basicConfig(  # filename='example.log',
     level=logging.DEBUG)
 
 # torch.manual_seed(123)
-np.random.seed(seed=42)
+# np.random.seed(seed=42)
 
 EPS = torch.tensor([1e-6])
 LOG2 = torch.log(torch.tensor([2.]))
 LOG2PI = torch.log(torch.tensor([2. * np.pi]))
 
 
-# TODO: Code doesn't work, needs debugging!!!
+# TODO: Implement ELBO and verify we maximize on each iteration
 # TODO: make priors explicit in class definition (currently they are just hardcoded in update equations)
+# TODO: clean up comments
+# TODO: better logging
+# TODO: cleanup asserts, verify correctness
 
 class VariationalGMM:
 
@@ -66,10 +69,65 @@ class VariationalGMM:
         self._gmm_z = torch.rand(m, self._k)
         self._gmm_z = self._gmm_z / torch.sum(self._gmm_z, dim=1).view(m, 1)
 
-    def log_likelihood(self, mus, vars, log_expected_pis):
-        pass
+        logging.debug("Initialized ELBO = {}".format(self.elbo()))
 
-    def expectation_step(self):
+    def elbo(self):
+        #
+        # ELBO = E_q [ log p(x, z, mu) ] - E_q [ log q(m, z) ]
+        #       = SUM_k E_q [ log p(mu_k) ] + SUM_i E_q [ log p(x | z, mu)] + E_q [ log p(z) ]
+        #       - SUM_k E_q [ log q(m) ] - SUM_i E_q [ log q(z) ]
+        #
+        # https://chrischoy.github.io/research/Expectation-Maximization-and-Variational-Inference-2/
+        # https://github.com/bertini36/GMM/blob/58bb1856115d54b470dd48bc7a9f78ff86304232/inference/autograd/gmm_gavi.py#L183
+        n, dim = self._data.size()
+
+        e_log_like = torch.zeros(self._k, 1)
+        e_log_psi = torch.zeros(self._k, 1)
+        e_log_pi = torch.zeros(self._k, 1)
+
+        q_mu_entropy = torch.zeros(self._k, 1)
+        q_pi_entropy = torch.zeros(n, 1)
+
+        for k in range(self._k):
+            e_log_like[k] = torch.sum(self._expectation_log_like(k))
+            e_log_psi[k] = self._expectation_log_psi(k)
+            e_log_pi[k] = self._expectation_log_pi(k, dim)
+
+            # entropy terms
+            q_mu_entropy[k] = (dim / 2) * np.log(2 * np.pi * np.e) + 0.5 * torch.log(torch.det(self._gmm_W[k, :, :]))
+
+        for i in range(n):
+            q_pi_entropy[i] = torch.sum(self._gmm_z[i, :] * torch.log(self._gmm_z[i, :]))
+
+        elbo = torch.sum(e_log_like) + torch.sum(e_log_psi) + torch.sum(e_log_pi) - torch.sum(q_mu_entropy) - torch.sum(q_pi_entropy)
+
+        logging.debug("ELBO: sum(e_log_like)   = {}".format(torch.sum(e_log_like)))
+        logging.debug("ELBO: sum(e_log_psi)    = {}".format(torch.sum(e_log_psi)))
+        logging.debug("ELBO: sum(e_log_pi)     = {}".format(torch.sum(e_log_pi)))
+        logging.debug("ELBO: sum(q_mu_entropy) = {}".format(torch.sum(q_mu_entropy)))
+        logging.debug("ELBO: sum(q_pi_entropy) = {}".format(torch.sum(q_pi_entropy)))
+        logging.debug("ELBO = {}".format(elbo))
+        return elbo
+
+    def _expectation_log_like(self, k):
+        """
+        Computation of E_q[(x-mu)^t * psi * (x - mu)]
+        """
+
+        n, dim = self._data.size()
+        e_gauss_k = torch.zeros(n, 1)
+
+        for i in range(n):
+            # break up terms to help with debugging
+            t1 = (-0.5) * d * torch.log(2 * np.pi * torch.det(self._gmm_W[k, :, :])) * self._gmm_z[i, k]
+
+            weighted_mean_diff = self._gmm_z[i, k] * (self._data[i, :] - self._gmm_m[k, :])
+            t2 = weighted_mean_diff.view(1, dim) @ torch.inverse(self._gmm_W[k, :, :]) @ (self._data[i, :] - self._gmm_m[k, :]).view(dim, 1)
+            e_gauss_k[i] = t1 - t2
+
+        return e_gauss_k
+
+    def maximization_step(self):
         """
         Expectation step of the variational EM algorithm.
         This step computes the expected value of the latent parameters (pi) which defines cluster ownership
@@ -109,7 +167,7 @@ class VariationalGMM:
             self._gmm_kappa[k] = 1 + n_k[k]
             self._gmm_m[k] = (torch.Tensor([1, 1]) + (self._gmm_z[:, k] @ self._data)) / self._gmm_kappa[k]
 
-    def maximization_step(self):
+    def expectation_step(self):
         """
         Maximization step of EM algorithm.
         This step optimizes the Gaussian parameters given the expected value of the latent variables (cluster ownership).
@@ -155,12 +213,51 @@ class VariationalGMM:
         self._gmm_z = _smooth(torch.exp(rho_star - z_exp_sum))
         assert torch.abs(torch.sum(self._gmm_z) - n) < 0.01, "Z's are not normalized"
 
+    # TODO use these function calls in maximization step
+    def _expectation_log_mu(self, k):
+        """
+        Computation of E_q[(x-mu)^t * psi * (x - mu)]
+        """
+
+        n, dim = self._data.size()
+        e_gauss_k = torch.zeros(n, 1)
+
+        for i in range(n):
+            # break up terms to help with debugging
+            t1 = (-0.5) * d / self._gmm_kappa[k]
+            t2 = (self._gmm_v[k] * (self._data[i, :].view(1, dim) - self._gmm_m[k, :].view(1, dim)))
+            t3 = self._data[i, :].view(d, 1) - self._gmm_m[k, :].view(dim, 1)
+            e_gauss_k[i] = t1 + t2 @ torch.inverse(self._gmm_W[k, :, :]) @ t3
+
+        return e_gauss_k
+
+    def _expectation_log_pi(self, k, data_dims):
+        """
+        Computation of E_q[log pi_k]
+        """
+        det = torch.det(torch.inverse(self._gmm_W[k, :, :]))
+        assert det >= 0, "W is not positive-definite!"
+
+        # E[log pi_k] ...
+        e_pi_k = d * LOG2 + torch.log(det)
+        for j in range(data_dims):
+            e_pi_k += torch.digamma((self._gmm_v[k] + 1 - j) / 2)
+
+        return e_pi_k
+
+    def _expectation_log_psi(self, k):
+        """
+        Computation of E_q [ log | psi_k| ]
+        """
+        e_psi_k = torch.digamma(self._gmm_alpha[k]) - torch.digamma(torch.sum(self._gmm_alpha))
+        return e_psi_k
+
     def expectation_maximization(self, max_iters=1000, converge_thresh=1e-3):
-        em_converged = False
-        log_likes = [-np.Inf]
+        is_converged = False
+        elbos = [-np.Inf]
         iters = 0
 
-        while not em_converged:
+        while not is_converged:
 
             #
             # Expectation
@@ -174,33 +271,28 @@ class VariationalGMM:
             # maximize parameters given expectations of pis
             self.maximization_step()
 
-            # # Compute log likelihood of the model to see how we're doing.
-            # # This should decrease over time.
-            # log_like = log_likelihood(data, mu_star, var_star, log_expected_pis)
-            # if np.abs(log_likes[-1] - log_like) < converge_thresh:
-            #     em_converged = True
-            #
-            # # track log likelihoods over time to logging purposes
-            # log_likes.append(log_like)
-            #
-            # # update parameters for next round
-            # mus = mu_star
-            # vars = var_star
-            #
-            # # perform check to try to correct when multiple clusters start converging to the same point
-            # mus = degenerative_check(mus)
+            # compute the ELBO to see how we're doing
+            elbo = self.elbo()
+
+            if elbo - elbos[-1] <= 0:
+                logging.warning("ELBO is decreasing!!!!")
+
+            if np.abs(elbos[-1] - elbo) < converge_thresh:
+                is_converged = True
+
+            elbos.append(elbo)
 
             iters += 1
-            logging.debug("EM iteration[{}]".format(iters))
-            if iters % 1 == 0:
+            logging.debug("Variational Inference iteration[{}], ELBO = {}".format(iters, elbo))
+            if iters % 2 == 0:
                 logging.debug("MU")
                 logging.debug(self._gmm_m)
 
-                n_k = torch.sum(self._gmm_z, dim=0).view(self._k, 1)
-                logging.debug("N[k] = {}".format(n_k))
-
-                logging.debug("COV")
-                logging.debug(self._gmm_W)
+                # n_k = torch.sum(self._gmm_z, dim=0).view(self._k, 1)
+                # logging.debug("N[k] = {}".format(n_k))
+                #
+                # logging.debug("COV")
+                # logging.debug(self._gmm_W)
 
             if iters > max_iters:
                 logging.debug('Breaking because we hit {} iterations'.format(max_iters))
@@ -256,7 +348,7 @@ if __name__ == '__main__':
     logging.debug("m = {}, d = {}".format(m, d))
 
     gmm = VariationalGMM(X, K)
-    mu_star, var_star, z_star = gmm.expectation_maximization(max_iters=10)
+    mu_star, var_star, z_star = gmm.expectation_maximization(max_iters=100)
 
     print("True parameters")
     print("Cluster means")
